73cd1fb74ac2691153a0cd6713456e4035f44cca
CelerinaD <21-14289@g.batstate-u.edu.ph>
2024-07-11 16:03:39 +0800

    ACTIVITY5

--- src/main/java/com/sample/Beaches/Model/Product.java
+++ src/main/java/com/sample/Beaches/Model/Product.java
@@ -12,49 +12,60 @@ public class Product {
     private String productName;
     private String description;
     private double price;
+    private String url;
 
     Product(){}
 
-    public Product(String productName, String description, double price) {
+    public Product(String productName, String description, double price, String url) {
         this.productName = productName;
         this.description = description;
         this.price = price;
+        this.url = url;
     }
 
-
-    public String getProductName() {
-        return productName;
+    public void setId(long id) {
+        this.id = id;
     }
-
 
     public void setProductName(String productName) {
         this.productName = productName;
     }
 
-
-    public String getDescription() {
-        return description;
-    }
-
-
     public void setDescription(String description) {
         this.description = description;
     }
-
-
-    public double getPrice() {
-        return price;
-    }
-
 
     public void setPrice(double price) {
         this.price = price;
     }
 
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
     public long getId() {
         return id;
     }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    
+
+
 
 
     
